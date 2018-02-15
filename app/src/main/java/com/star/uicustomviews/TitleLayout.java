2016b18b818664344f734723240ce4089f09bc8e
crystalDf <chendong333@gmail.com>
2018-02-15 13:14:40 +0800

    Chapter 03 UICustomViews

--- app/src/main/java/com/star/uicustomviews/TitleLayout.java
+++ app/src/main/java/com/star/uicustomviews/TitleLayout.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -19,20 +18,10 @@ public class TitleLayout extends LinearLayout {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.title, this);
 
-        mTitleBack = (Button) findViewById(R.id.title_back);
-        mTitleBack.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((Activity) getContext()).finish();
-            }
-        });
+        mTitleBack = findViewById(R.id.title_back);
+        mTitleBack.setOnClickListener(v -> ((Activity) getContext()).finish());
 
-        mTitleEdit = (Button) findViewById(R.id.title_edit);
-        mTitleEdit.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "Edit Button", Toast.LENGTH_LONG).show();
-            }
-        });
+        mTitleEdit = findViewById(R.id.title_edit);
+        mTitleEdit.setOnClickListener(v -> Toast.makeText(getContext(), "Edit Button", Toast.LENGTH_LONG).show());
     }
 }
